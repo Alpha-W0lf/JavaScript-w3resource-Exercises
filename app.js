e7d20ebbb5f655779f491754bbc534f1639cdb9e
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-09-07 19:50:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+document.write("<br/>");
+
 // 1. Write a JavaScript program to display the current day and time in the following format.  Go to the editor
 // Sample Output : Today is : Tuesday.
 // Current time is : 10 PM : 30 : 38
@@ -26,6 +28,12 @@
 // 3. Write a JavaScript program to get the current date.  Go to the editor
 // Expected Output :
 // mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+
+
+let today = new Date();
+
+document.write(today);
+
 
 
 // 4. Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.  Go to the editor
