7f4026247bb9d7832c1f4f325026b98225096ea0
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-09-09 16:17:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -236,6 +236,13 @@ function nameVar() {
 
 // 14. Write a JavaScript exercise to get the extension of a filename.  Go to the editor
 
+function getExtension() {
+
+
+}
+
+getExtension()
+
 
 // 15. Write a JavaScript program to get the difference between a given number and 13, if the number is greater than 13 return double the absolute difference.  Go to the editor
 
